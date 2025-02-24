c961ec1a6cbbbdcb1455afd768914dbdd8dda87a
meherab111 <mehrabborno@gmail.com>
2025-02-25 02:15:35 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,57 +1,56 @@
+// Dummy Object With Data
+
 const studentData = {
+  name: "Araf Mahmud",
 
-    name: "Araf Mahmud",
+  education: "Honours 3rd Year",
 
-    education: "Honours 3rd Year",
+  age: 23,
 
-    age: 23,
-
-    address: {
-        city: "uttara",
-        district: "Dhaka",
-        postCode: 1230
-    }
-
+  address: {
+    city: "uttara",
+    district: "Dhaka",
+    postCode: 1230,
+  },
 };
 
 let button1 = document.getElementById("btn-1");
 let button2 = document.getElementById("btn-2");
 let button3 = document.getElementById("btn-3");
-let showData = document.getElementById("data_show");
+let showData = document.getElementById("data-show");
 
+// Setting Data To Local Storage
 
 const funcBtn1 = () => {
+  let jsonConvert = JSON.stringify(studentData);
 
-    let jsonConvert = JSON.stringify(studentData);
+  localStorage.setItem("studentData", jsonConvert);
+  alert("Data Added");
+};
 
-    localStorage.setItem("studentData", jsonConvert);
-    alert("Data Added");
-}
+// Getting Data From Local Storage
 
 const funcBtn2 = () => {
+  const getData = JSON.parse(localStorage.getItem("studentData"));
 
+  let pTag = document.createElement("p");
 
-    const getData = JSON.parse(localStorage.getItem("studentData"));
+  pTag.classList.add("pTag");
 
-    let pTag = document.createElement("p");
+  pTag.innerHTML = `Data From Local Storage : name: ${getData.name}, education: ${getData.education}, city: ${getData.address.city}, district: ${getData.address.district}, postCode: ${getData.address.postCode}`;
 
-    pTag.classList.add("pTag");
+  showData.append(pTag);
+};
 
-    pTag.innerHTML = `Data From Local Storage : name: ${getData.name}, education: ${getData.education}, city: ${getData.address.city}, district: ${getData.address.district}, postCode: ${getData.address.postCode}`;
-    
-    showData.append(pTag);
-
-    
-    
-}
+// Removing Data From Local Storage
 
 const funcBtn3 = () => {
+  localStorage.removeItem("studentData");
+  alert("Data Removed");
+  showData.innerHTML = "";
+};
 
-    localStorage.removeItem("studentData");
-    alert("Data Removed");
-    showData.innerHTML = "";
-}
-
+// Click Events
 
 button1.addEventListener("click", funcBtn1);
 button2.addEventListener("click", funcBtn2);
